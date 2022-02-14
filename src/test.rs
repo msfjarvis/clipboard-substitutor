ff3a72968ab55b5b59204bbc37c9cd76076f5420
Harsh Shandilya <me@msfjarvis.dev>
2022-02-15 02:59:54 +0530

    feat: add tests for actions

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,4 @@
-use crate::config::{Match, Matcher};
+use crate::config::{Act, Action, Match, Matcher};
 
 #[test]
 fn regex_matcher() {
@@ -7,4 +7,31 @@ fn regex_matcher() {
     };
     assert!(matcher.check_match("https://example.com"));
     assert!(!matcher.check_match("example.com"));
+}
+
+#[test]
+fn set_action() {
+    let action = Action::Set { content: "doe" };
+    assert_eq!("doe", action.apply_action("john"));
+}
+
+#[test]
+fn replace_action() {
+    let action = Action::Replace {
+        from: "doe",
+        to: "bow",
+    };
+    assert_eq!("john bow", action.apply_action("john doe"));
+}
+
+#[test]
+fn prefix_action() {
+    let action = Action::Prefix { prefix: "hello " };
+    assert_eq!("hello john", action.apply_action("john"));
+}
+
+#[test]
+fn suffix_action() {
+    let action = Action::Suffix { suffix: " doe" };
+    assert_eq!("john doe", action.apply_action("john"));
 }
