b67f8e4b56b17180807823342551376fd1e7abff
Harsh Shandilya <me@msfjarvis.dev>
2022-02-19 00:19:48 +0530

    Revert "nix: pin Rust version"
    This reverts commit 0385f033b22a4b79c3379042b87cae1d0588a412.

--- shell.nix
+++ shell.nix
@@ -2,9 +2,10 @@ with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   RUSTFLAGS = "";
   buildInputs = [
-    (rust-bin.nightly."2022-02-14".default.override {
-      extensions = [ "rust-src" "rustfmt-preview" ];
-    })
+    (rust-bin.selectLatestNightlyWith (toolchain:
+      toolchain.default.override {
+        extensions = [ "rust-src" "rustfmt-preview" ];
+      }))
     xorg.libxcb
   ];
 }
