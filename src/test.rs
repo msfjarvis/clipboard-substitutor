8e2f68b9b76e3d4f604d64d046e66de7202e36a9
Harsh Shandilya <me@msfjarvis.dev>
2022-02-20 02:57:22 +0530

    feat: add tests for config parsers

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,4 @@
-use crate::config::{Act, Action, Match, Matcher};
+use crate::config::{Act, Action, Match, Matcher, MatcherType, Replacements};
 use assay::assay;
 
 #[assay]
@@ -35,4 +35,39 @@ fn prefix_action() {
 fn suffix_action() {
     let action = Action::Suffix { suffix: " doe" };
     assert_eq!("john doe", action.apply_action("john"));
+}
+
+#[assay]
+fn parse_with_multiple_matchers() {
+    let config = r#"
+    [[substitutor]]
+    name = "Example"
+    matcher = [
+        { starts_with = { prefix = "https://example.com" } },
+        { ends_with = { suffix = ".mp4" } }
+    ]
+    action = { prefix = { prefix = "/mirror" } }
+    "#;
+    let config: Replacements<'_> = toml::from_str(config)?;
+    assert_eq!(1, config.substitutors.len());
+    let subst = &config.substitutors[0];
+    assert_eq!("Example", subst.name);
+    assert!(matches!(subst.matcher_type, MatcherType::Multiple(_)));
+    assert!(matches!(subst.action, Action::Prefix { .. }));
+}
+
+#[assay]
+fn parse_with_single_matcher() {
+    let config = r#"
+    [[substitutor]]
+    name = "Example"
+    matcher = { starts_with = { prefix = "https://example.com" } }
+    action = { prefix = { prefix = "/mirror" } }
+    "#;
+    let config: Replacements<'_> = toml::from_str(config)?;
+    assert_eq!(1, config.substitutors.len());
+    let subst = &config.substitutors[0];
+    assert_eq!("Example", subst.name);
+    assert!(matches!(subst.matcher_type, MatcherType::Single(_)));
+    assert!(matches!(subst.action, Action::Prefix { .. }));
 }
