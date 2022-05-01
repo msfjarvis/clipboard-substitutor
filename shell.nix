1a1e97722b66f5501667136670ad855d5560fa47
Harsh Shandilya <me@msfjarvis.dev>
2022-05-01 18:06:07 +0530

    feat: switch nix to GNU triple

--- shell.nix
+++ shell.nix
@@ -3,12 +3,12 @@ mkShell {
   buildInputs = [
     (rust-bin.selectLatestNightlyWith (toolchain:
       toolchain.default.override {
-        extensions = [ "rust-src" "rustfmt-preview" "llvm-tools-preview"];
-      targets =
-        pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
-        ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-        [ "x86_64-unknown-linux-musl" ];
-    }))
+        extensions = [ "rust-src" "rustfmt-preview" "llvm-tools-preview" ];
+        targets =
+          pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+          [ "x86_64-unknown-linux-gnu" ];
+      }))
     xorg.libxcb
     python39
   ];
