b52dea88b0c1a84328c60604c32f711fb1d74e01
Harsh Shandilya <me@msfjarvis.dev>
2024-12-16 18:33:26 +0530

    chore: reformat all files with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -22,83 +22,101 @@
   inputs.flake-compat.url = "github:nix-community/flake-compat";
   inputs.flake-compat.flake = false;
 
-  outputs = {
-    self,
-    nixpkgs,
-    advisory-db,
-    crane,
-    devshell,
-    fenix,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [devshell.overlays.default];
-      };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      advisory-db,
+      crane,
+      devshell,
+      fenix,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
 
-      rustStable = (import fenix {inherit pkgs;}).fromToolchainFile {
-        file = ./rust-toolchain.toml;
-        sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
-      };
+        rustStable = (import fenix { inherit pkgs; }).fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+        };
 
-      craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
-      commonArgs = {
-        src = craneLib.cleanCargoSource ./.;
-        buildInputs = [];
-        nativeBuildInputs = with pkgs;
-          [xorg.libxcb python312]
-          ++ pkgs.lib.optionals stdenv.isDarwin
-          [pkgs.darwin.apple_sdk.frameworks.AppKit];
-        cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-      };
-      cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {doCheck = false;});
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = [ ];
+          nativeBuildInputs =
+            with pkgs;
+            [
+              xorg.libxcb
+              python312
+            ]
+            ++ pkgs.lib.optionals stdenv.isDarwin [ pkgs.darwin.apple_sdk.frameworks.AppKit ];
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+        };
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { doCheck = false; });
 
-      clipboard-substitutor = craneLib.buildPackage (commonArgs // {doCheck = false;});
-      clipboard-substitutor-clippy = craneLib.cargoClippy (commonArgs
-        // {
-          inherit cargoArtifacts;
-        });
-      clipboard-substitutor-fmt = craneLib.cargoFmt (commonArgs // {});
-      clipboard-substitutor-audit = craneLib.cargoAudit (commonArgs // {inherit advisory-db;});
-      clipboard-substitutor-nextest = craneLib.cargoNextest (commonArgs
-        // {
-          inherit cargoArtifacts;
-          partitions = 1;
-          partitionType = "count";
-        });
-    in {
-      checks = {
-        inherit clipboard-substitutor clipboard-substitutor-audit clipboard-substitutor-clippy clipboard-substitutor-fmt clipboard-substitutor-nextest;
-      };
-
-      packages.default = clipboard-substitutor;
-
-      apps.default = flake-utils.lib.mkApp {drv = clipboard-substitutor;};
-
-      devShells.default = pkgs.devshell.mkShell {
-        imports = [
-          "${devshell}/extra/language/c.nix"
-          "${devshell}/extra/language/rust.nix"
-        ];
-
-        env = [
-          {
-            name = "DEVSHELL_NO_MOTD";
-            value = 1;
+        clipboard-substitutor = craneLib.buildPackage (commonArgs // { doCheck = false; });
+        clipboard-substitutor-clippy = craneLib.cargoClippy (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
           }
-        ];
+        );
+        clipboard-substitutor-fmt = craneLib.cargoFmt (commonArgs // { });
+        clipboard-substitutor-audit = craneLib.cargoAudit (commonArgs // { inherit advisory-db; });
+        clipboard-substitutor-nextest = craneLib.cargoNextest (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            partitions = 1;
+            partitionType = "count";
+          }
+        );
+      in
+      {
+        checks = {
+          inherit
+            clipboard-substitutor
+            clipboard-substitutor-audit
+            clipboard-substitutor-clippy
+            clipboard-substitutor-fmt
+            clipboard-substitutor-nextest
+            ;
+        };
 
-        packages = with pkgs; [
-          cargo-dist
-          cargo-nextest
-          cargo-release
-          rustStable
-        ];
+        packages.default = clipboard-substitutor;
 
-        language.c.libraries = commonArgs.nativeBuildInputs;
-        language.rust.enableDefaultToolchain = false;
-      };
-    });
+        apps.default = flake-utils.lib.mkApp { drv = clipboard-substitutor; };
+
+        devShells.default = pkgs.devshell.mkShell {
+          imports = [
+            "${devshell}/extra/language/c.nix"
+            "${devshell}/extra/language/rust.nix"
+          ];
+
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+
+          packages = with pkgs; [
+            cargo-dist
+            cargo-nextest
+            cargo-release
+            rustStable
+          ];
+
+          language.c.libraries = commonArgs.nativeBuildInputs;
+          language.rust.enableDefaultToolchain = false;
+        };
+      }
+    );
 }
